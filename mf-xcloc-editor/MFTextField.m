a86276af1ea732b7aebf134907e3ee499799d298
Noah Nuebling <noah.n.public@gmail.com>
2025-10-24 02:03:16 +0200

    Note added

--- mf-xcloc-editor/MFTextField.m
+++ mf-xcloc-editor/MFTextField.m
@@ -7,8 +7,10 @@
 
 ///
 /// NSTextField that auto-resizes while being edited.
+///     NSTextField doesn't have intrinsicHeight when editable. (Which means it won't work right with autolayout) This does have intrinsicHeight..
+///     Used in the `@target"` column of `TableView` [Oct 2025]
 ///     Discussion: For the ToastNotifications in MMF I reverse-engineered how NSTextField does its auto-layout-support and decided it was too hard and went with programmatic layout instead.
-///         But this seems to work well (at least embedded in an `NSTableView` - where the width is fixed to the column width) [Oct 2025]
+///         But this seems to work well (at least embedded in an `NSTableView` - where the width is fixed to the column width, which may make things easier) [Oct 2025]
 ///
 
 #import "MFTextField.h"
@@ -58,7 +60,7 @@
             }];
         
             newResult = [attrStr sizeAtMaxWidth: self.frame.size.width - 4.0]; /// Has to be exactly -4.0. Not sure why. This might be `self.currentEditor.textContainer.lineFragmentPadding * 2` [Oct 2025]
-            newResult.height += 2.0; /// Without `+2`, the content shifts slightly up/down when MFTextField is in the tallest row and you add / remove lines from it. [Oct 2025]
+            newResult.height += 2.0; /// Without `+2`, the content shifts slightly up/down when MFTextField is in the tallest cell in the row and you add / remove text lines from it. [Oct 2025]
         }
         return newResult;
     }
