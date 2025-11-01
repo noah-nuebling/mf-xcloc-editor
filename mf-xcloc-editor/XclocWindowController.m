abf450867ad6bd97930c7cd22f95a5c8e9a11684
Noah Nuebling <noah.n.public@gmail.com>
2025-11-01 14:29:13 +0100

    Check off todo

--- mf-xcloc-editor/XclocWindowController.m
+++ mf-xcloc-editor/XclocWindowController.m
@@ -151,7 +151,7 @@
             /// TODO:
             /// - [x] Fix autolayout crash when toggling state while the cell is off-screen and then resizing the window so it's on-screen. (Fixed by using reloadItem:)
             /// - [x] Change icon back
-            /// - [ ] Maybe implement glyphs for single-linebreak (/ other invisibles)
+            /// - [x] Maybe implement glyphs for single-linebreak (/ other invisibles)
         
             /// TODO: Fix issue where double-clicking / triple-clicking /...  a row does nothing (instead of starting text-editing)
             ///     Tried to fix this but hard. Maybe just live with it.
