b89354aa6e77e1d5438d46eca080394b14271dad
Noah Nuebling <noah.n.public@gmail.com>
2025-11-02 14:03:51 +0100

    Add searchSubviewWithIdentifier: utility in NSView+Additions.h/.m

--- mf-xcloc-editor/Utility/NSView+Additions.h
+++ mf-xcloc-editor/Utility/NSView+Additions.h
@@ -7,10 +7,8 @@
 
 #import <Cocoa/Cocoa.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface NSView (Additions)
 
-@end
+    - (NSView *) searchSubviewWithIdentifier: (NSUserInterfaceItemIdentifier) identifier;
 
-NS_ASSUME_NONNULL_END
+@end
--- mf-xcloc-editor/Utility/NSView+Additions.m
+++ mf-xcloc-editor/Utility/NSView+Additions.m
@@ -9,4 +9,19 @@
 
 @implementation NSView (Additions)
 
+    - (NSView *) searchSubviewWithIdentifier: (NSUserInterfaceItemIdentifier) identifier {
+        
+        /// This recursively searches the view's subviews. For single-level search we tend to use the `firstmatch` macro. [Nov 2025]
+        
+        NSView *rec;
+        
+        for (NSView *v in self.subviews) {
+            if ([[v identifier] isEqual: identifier]) return v;
+            if ((rec = [v searchSubviewWithIdentifier: identifier]))
+                return rec;
+        }
+        
+        return nil;
+    }
+
 @end
