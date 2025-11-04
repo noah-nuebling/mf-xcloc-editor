b15ca6236e23edd660aa4b2d817b516c5e942d03
Noah Nuebling <noah.n.public@gmail.com>
2025-11-04 09:25:47 +0100

    Delete finished TODOs

--- mf-xcloc-editor/XclocWindowController.m
+++ mf-xcloc-editor/XclocWindowController.m
@@ -152,15 +152,9 @@
         {
             
             /// TODO: Make default width on small laptop screen better
-            /// TODO: Add "Show in 'filename'" to right-click menu. (DONE)
             
             /// TODO: Fix Command-J while editing selection
-            /// TODO: Maybe Make Command-J -> Command-G so all the shortcuts are reachable through lefthand? (NO)
             /// TODO: Disable type-to-search on the TableView
-                
-            /// TODO: Don't automatically set state to 'translated' unless the user stops editing via return key. (DONE)
-            /// TODO: (DONE)
-            ///     Bug: `72q-sS-xMS.​title` -> Cycle through states changes comment
             
         
             /// Abandoned TODOs:
