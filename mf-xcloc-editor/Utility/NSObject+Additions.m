ba395ff198df4dbc9787412670c99999f8893df0
Noah Nuebling <noah.n.public@gmail.com>
2025-12-12 19:11:26 +0100

    Comment added + slight change in mf_associatedObjects

--- mf-xcloc-editor/Utility/NSObject+Additions.m
+++ mf-xcloc-editor/Utility/NSObject+Additions.m
@@ -25,11 +25,13 @@
     - (NSMutableDictionary *)mf_associatedObjects {
     
         /// associatedObject convenience. Very useful. You can do all the JavaScript things. [Dec 2025]
+        
+        static const char *key = "__mf_associatedObjects"; /// Is this shared across compilation units? (Doesn't matter since mf-xcloc-editor is a unity-build now.) (We could hash the string or use @selector if this ever causes problems) [Dec 2025]
     
-         id dict = objc_getAssociatedObject(self, "__mf_associatedObjects");
+         id dict = objc_getAssociatedObject(self, key);
          if (!dict) {
             dict = [NSMutableDictionary new];
-            objc_setAssociatedObject(self, "__mf_associatedObjects", dict, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+            objc_setAssociatedObject(self, key, dict, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
          }
          return dict;
     }
